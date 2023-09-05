903ca75d2dd2acf03ca206ab9c10c315582c2380
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-09-05 11:25:35 -0300

    Create saque_dinheiro.php

---
+++ saque_dinheiro.php
@@ -0,0 +1,38 @@
+<?php
+// Enter your code here, enjoy!
+$saque = 1020;
+$resto = 0;
+
+$sem = (int)($saque / 100);
+$resto = $saque % 100;
+
+
+
+$cinquenta = (int)($resto/50);
+$resto = $resto% 50;
+
+$vinte = (int)($resto/20);
+$resto = $resto% 20;
+
+$dez = (int)($resto/10);
+$resto = $resto% 10;
+
+$cinco = (int)($resto/5);
+$resto = $resto% 5;
+
+$dois = (int)($resto/2);
+$resto = $resto% 2;
+
+$um = (int)($resto/1);
+$resto = $resto% 1;
+
+
+
+echo "sedas de 100 $sem <br>
+sedas de cinqueta $cinquenta <br>
+sedas de 20 $vinte <br>
+sedas de dez $dez <br>
+sedas das de cinco $cinco <br>
+sedas de dois $dois <br>
+sedas de um $um <br>
+";
