145e94a4744887919c3020909f88bc6551912376
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-13 12:59:20 -0300

    Create formatar-cpf.php

---
+++ formatar-cpf.php
@@ -0,0 +1,34 @@
+//https://gist.github.com/marceloneppel/dd9c17a01c1a8031c760b034dad0efd9
+//PHP
+
+<?php
+function formatCnpjCpf($value)
+{
+  $cnpj_cpf = preg_replace("/\D/", '', $value);
+  
+  if (strlen($cnpj_cpf) === 11) {
+    return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
+  } 
+  
+  return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $cnpj_cpf);
+}
+
+
+//JS
+
+
+
+function formatCnpjCpf(value)
+{
+  const cnpjCpf = value.replace(/\D/g, '');
+  
+  if (cnpjCpf.length === 11) {
+    return cnpjCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "\$1.\$2.\$3-\$4");
+  } 
+  
+  return cnpjCpf.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g, "\$1.\$2.\$3/\$4-\$5");
+}
+
+var resu = formatCnpjCpf('15896578439');
+
+console.log(resu);
