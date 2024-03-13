ce6625d882a12f2f078d304520239b9b0a5a4cc1
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-13 13:00:48 -0300

    Create array-teste.php

---
+++ array-teste.php
@@ -0,0 +1,11 @@
+<?php
+//https://www.php.net/manual/pt_BR/function.print-r.php
+
+  $arr = array(nome=>"talilo", sobrenome=>"tarlison");
+
+  $n = array(2,3,5);
+  
+	print_r($n);
+	print_r($arr)
+	
+?>
