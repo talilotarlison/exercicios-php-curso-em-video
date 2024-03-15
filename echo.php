2e5ccc6339c060be31c65dd2540dd2c42581b829
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-15 17:53:19 -0300

    Create echo.php

---
+++ echo.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<style>
+		body{
+			background-color: red;
+			color: red;
+		}
+	</style>
+<title>Teste PHP</title>
+</head>
+<body>
+<?php echo "<p>Olá Mundo</p>"; ?>
+</body>
+</html>
