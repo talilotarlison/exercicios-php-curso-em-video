8a8ff2a17e393a7fe9ac31a764ab2a91863cc766
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-16 13:14:23 -0300

    Add files via upload

---
+++ get-Localizacao.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+ 
+<head lang="pt-br">
+    <title>Meu primeiro script</title>
+    <meta charset="utf-8">
+ 
+    <!-- estilos -->
+    <link rel="stylesheet" href="estilo.css">
+ 
+    <!-- scripts -->
+  <!--  <script src="script.js"></script>-->
+</head>
+ 
+<body>
+    <!-- conteúdo da página -->
+	<div></div>
+	<?php
+	echo "hello";
+	?> 
+</body>
+
+<script>
+/*
+var lat;
+var lon;
+ // geolocalização na pagina web
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(
+    function(posicao){
+      console.log(posicao);
+ 
+      lat = posicao.coords.latitude;
+      lon = posicao.coords.longitude;
+ 
+      console.log(lat, lon);
+ 
+    } 
+  );
+}
+
+
+
+*/
+</script>
+ <script src='./resultado.js' type="module"></script>
+</html>
+
