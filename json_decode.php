a0d83dd4c77fe4e2e8e2cb0ceb938a4b34a8a729
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-04-05 15:57:48 -0300

    teste

---
+++ json_decode.php
@@ -0,0 +1,10 @@
+<?php
+
+// https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Objects/JSON
+// https://www.w3schools.com/Php/func_json_decode.asp
+
+$jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+ 
+var_dump(json_decode($jsonobj));
+
+?>
