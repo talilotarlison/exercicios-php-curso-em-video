67f34ae019d702b22173edeb1b872731b3faad93
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-04-08 11:11:21 -0300

    atualização

---
+++ variaveis.php
@@ -0,0 +1,22 @@
+
+<?php
+// declaração de variaveis em php
+ // variavel referênciada
+ 
+$a = 5;
+$b = &$a;
+$b += 7;
+
+	echo "valor de  b : $b \nvalor de a : $a\n";
+	
+// variável de variável	
+	
+	$nome = "talilo";
+	$$nome = "joao";
+	
+	echo "$nome \n";
+	
+	echo "$talilo";
+	
+	
+?>
--- variaveis.py
+++
@@ -1,4 +0,0 @@
-# declaração de variaveis em ptyhon
-a , b = 1 ,2
-
-print(b)
