2a08005f54faef95ef3162ef19b6836fe0add7ba
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-04-08 11:29:24 -0300

    Create variavel-ref.php

---
+++ variavel-ref.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// variável de variável	
+	
+	// teste 01
+	$nome = "talilo";
+	$$nome = "joao";
+	
+	echo "$nome \n";
+	
+	echo "$talilo \n";
+	
+	// teste 02
+	$num = "dez";
+	$$num = "vinte";
+	
+	echo "$dez";
+	
+	
+?>
