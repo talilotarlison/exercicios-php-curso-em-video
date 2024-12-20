4f27ca1b91595135be07974374d28703e86e203f
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-12-20 20:31:41 -0300

    Create array-matrix.php

---
+++ array-matrix.php
@@ -0,0 +1,13 @@
+<?php
+// array 
+	$arr = [2,3,5,3];
+	var_dump($arr);
+	// array 
+	$arr1 = Array(2,3,4);
+	var_dump($arr1);
+	// matrix
+	$arr3 = [[2,3],
+	          [3,2],
+	          [5,3]];
+	var_dump($arr3);
+?>
