b2ead6bf92076637f19ca7b05bc3a4c89c3ff4aa
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-12-19 22:59:39 -0300

    Create for-php.php

---
+++ for-php.php
@@ -0,0 +1,17 @@
+<?php
+
+  //https://cursos.alura.com.br/forum/topico-quebra-de-linha-com-php_eol-163093
+  
+  $multiplicador = 6;
+  
+  for ($i=1; $i<=10; $i++) {
+      $resultado = $multiplicador * $i;
+      echo "{$multiplicador} x {$i} = {$resultado}\n";
+  }
+
+
+  
+  for ($i=1; $i<=10; $i++) {
+      echo " Select - $i \n";
+  }
+?>
